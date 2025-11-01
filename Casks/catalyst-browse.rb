e8fe40becad6bf07f1eb9d8efa9e182ba7a7a4d0
gz4zzxc <33158166+gz4zzxc@users.noreply.github.com>
2025-11-01 17:53:00 +0800

    cask catalyst-browse: revert to 2025.2

--- Casks/catalyst-browse.rb
+++ Casks/catalyst-browse.rb
@@ -1,5 +1,5 @@
 cask "catalyst-browse" do
-  version "2025.3"
+  version "2025.2"
   sha256 "24bf06358ea7b9c359b934599786a5f9b97e970b7ebbcc3685d019c2c2bbadf1"
 
   url "https://di.update.sony.net/NEX/b579a2667c/Catalyst_Browse.dmg",
