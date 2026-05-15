0f3ff95c83111cdbaf5b51bc31890d35d6e93593
gz4zzxc <33158166+gz4zzxc@users.noreply.github.com>
2026-05-15 20:47:12 +0800

    cask futu-niuniu: update to 16.15.16008

--- Casks/futu-niuniu.rb
+++ Casks/futu-niuniu.rb
@@ -2,13 +2,14 @@ cask "futu-niuniu" do
   version "16.15.16008"
   sha256 "9a4252390fc9c6b861f3d50e2eb93a7891cbbe5a8b642b048f106b69fff44bf8"
 
-  url "https://softwaredownload.futunn.com/FTNN_desktop_#{version}_Website.dmg",
-      verified: "softwaredownload.futunn.com/"
+  url "https://softwaredownload.futunn.com/FTNN_desktop_#{version}_Website.dmg"
   name "Futu Niuniu"
   name "富途牛牛"
   desc "富途证券官方交易软件，支持港股、美股、A股交易"
   homepage "https://www.futunn.com/"
+
   auto_updates true
+  depends_on macos: :catalina
 
   app "富途牛牛.app"
 
